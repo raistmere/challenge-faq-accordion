6d70d0a8568b6c08872807106e36747d29b4c9b1
raistmere <raistmere@gmail.com>
2024-01-15 18:20:11 -0600

    Adding font family to question.module.css

--- src/components/Question/Question.tsx
+++ src/components/Question/Question.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import style from "./Question.module.css";
-import plusIcon from "../../assets/images/icon-plus.svg";
+import plusIconImg from "../../assets/images/icon-plus.svg";
+import minusIconImg from "../../assets/images/icon-minus.svg";
 
 interface Props {
     question : String;
@@ -21,7 +22,7 @@ const Question = (props : Props) => {
             <div className={style.wrapper}>
                 <button className={style.questionBox} aria-label="open" onClick={changeHiddenState}>
                     <p>{props.question}</p>
-                    <img src={plusIcon} alt="A Plus Icon" />
+                    <img src={plusIconImg} alt="A Plus Icon" />
                 </button>
                 <div className={style.answerBox}>
                     <p className={style.hidden}>{props.answer}</p>
@@ -34,7 +35,7 @@ const Question = (props : Props) => {
             <div className={style.wrapper}>
                 <button className={style.questionBox} aria-label="close" onClick={changeHiddenState}>
                     <p>{props.question}</p>
-                    <img src="src/assets/images/icon-minus.svg" alt="A Minus Icon" />
+                    <img src={minusIconImg} alt="A Minus Icon" />
                 </button>
                 <div className={style.answerBox}>
                     <p>{props.answer}</p>
